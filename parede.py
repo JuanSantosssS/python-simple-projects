9e1db6b47bbff0182d7939f7c301a6467a5bd7e2
JuanSantosssS <tftjuan5@gmail.com>
2024-04-25 16:02:32 -0300

    Create parede.py

---
+++ parede.py
@@ -0,0 +1,6 @@
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da Parede: '))
+área = larg * alt
+print ('Sua área é de {}M '.format(área))
+tinta = área / 2
+print ('Você precisara de {}Lt de tinta'.format(tinta))
